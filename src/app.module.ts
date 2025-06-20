0fc6265132f9aa7e0f888104c33e69b8dba1047b
zakkiislamii <mzislami05@gmail.com>
2025-06-21 00:36:52 +0700

    tes endpoint get all user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './services/app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './repositories/user/user.module';
 import { UserController } from './controllers/user.controller';
-import { UserService } from './services/user.service';
 
 @Module({
   imports: [DatabaseModule, UserModule],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/common/interfaces/response.interface.ts
@@ -0,0 +1,11 @@
+export interface ApiSuccessResponse<T = any> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+export interface ApiErrorResponse {
+  statusCode: number;
+  message: string;
+  error: string;
+}
